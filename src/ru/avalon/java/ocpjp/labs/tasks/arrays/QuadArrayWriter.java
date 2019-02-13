de7bd191cedf5fd27057331b2014ae14b9343a3b
Ulanov Ivan <veloteh@outlook.com>
2019-02-14 00:06:06 +0300

    Second

---
+++ src/ru/avalon/java/ocpjp/labs/tasks/arrays/QuadArrayWriter.java
@@ -0,0 +1,17 @@
+package ru.avalon.java.ocpjp.labs.tasks.arrays;
+
+import ru.avalon.java.ocpjp.labs.common.ObjectWriter;
+
+import java.io.IOException;
+
+public class QuadArrayWriter implements ObjectWriter {
+    @Override
+    public void write(Object object) throws IOException {
+
+    }
+
+    @Override
+    public void close() throws IOException {
+
+    }
+}
--- src/ru/avalon/java/ocpjp/labs/tasks/arrays/RandomQuadArray.java
+++ src/ru/avalon/java/ocpjp/labs/tasks/arrays/RandomQuadArray.java
@@ -4,17 +4,17 @@ import ru.avalon.java.ocpjp.labs.common.Factory;
 
 import java.util.Random;
 
-public class RandomArrayFactory implements Factory<int[]> {
-
+public class RandomQuadArray implements Factory {
 
     private final Random RANDOM = new Random();
 
-
     @Override
-    public int[] create() {
-        int[] array = new int[RANDOM.nextInt(10) + 10];
+    public Object create() {
+        int[][] array = new int[RANDOM.nextInt(10) + 10][RANDOM.nextInt(20) + 20];
         for (int i = 0; i < array.length; i++) {
-            array[i] = RANDOM.nextInt(100);
+            for (int j = 0; j < array[i].length; j++) {
+
+            }
         }
         return array;
     }
